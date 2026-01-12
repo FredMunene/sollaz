16bb02bafaa84f45730677285f81a134a64daa1a
fredmunene <mfredgitonga@gmail.com>
2026-01-12 22:23:02 +0300

    chore: scaffold vite project config

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+
+export default defineConfig({
+  plugins: [
+    nodePolyfills(),
+    react()
+  ],
+  server: {
+    allowedHosts: ['configurationally-interfascicular-shandra.ngrok-free.dev']
+  }
+});
